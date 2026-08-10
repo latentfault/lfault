ff645b75a1a1b8d33f0dfe97893d32c8724da854
latentfault <latentfault@proton.me>
2026-08-10 15:40:45 +0200

    add main function

--- lfault.py
+++ lfault.py
@@ -5,34 +5,40 @@ HOST = "127.0.0.1"
 PORT = 8080
 BUFFER_SIZE = 4096
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((HOST, PORT))
-    server.listen()
 
-    print(f"[+] lfault listening on {HOST}:{PORT}")
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind((HOST, PORT))
+        server.listen()
 
-    client, address = server.accept()
+        print(f"[+] lfault listening on {HOST}:{PORT}")
 
-    with client:
-        print(f"[+] connection from {address[0]}:{address[1]}")
+        client, address = server.accept()
 
-        request = b""
+        with client:
+            print(f"[+] connection from {address[0]}:{address[1]}")
 
-        while b"\r\n\r\n" not in request:
-            chunk = client.recv(BUFFER_SIZE)
+            request = b""
 
-            if not chunk:
-                break
+            while b"\r\n\r\n" not in request:
+                chunk = client.recv(BUFFER_SIZE)
 
-            request += chunk
+                if not chunk:
+                    break
 
-        print("[+] incoming request:")
-        print(request.decode("iso-8859-1").rstrip())
+                request += chunk
 
-        client.sendall(
-            b"HTTP/1.1 501 Not Implemented\r\n"
-            b"Content-Length: 0\r\n"
-            b"Connection: close\r\n"
-            b"\r\n"
-        )
+            print("[+] incoming request:")
+            print(request.decode("iso-8859-1").rstrip())
+
+            client.sendall(
+                b"HTTP/1.1 501 Not Implemented\r\n"
+                b"Content-Length: 0\r\n"
+                b"Connection: close\r\n"
+                b"\r\n"
+            )
+
+
+if __name__ == "__main__":
+    main()
