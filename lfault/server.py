c5f8e2faf820c06e41ea531b174321d3e79385a6
latentfault <latentfault@proton.me>
2026-09-21 16:53:47 +0200

    allow socket reuse

--- lfault/server.py
+++ lfault/server.py
@@ -3,6 +3,7 @@ import socket
 
 def listen(port=8080):
     listener = socket.socket()
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listener.bind(("127.0.0.1", port))
     listener.listen()
     return listener
