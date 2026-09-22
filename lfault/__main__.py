45ab06dac8540fac1e5ec3988bb3b104519a514c
latentfault <latentfault@proton.me>
2026-09-22 10:59:02 +0200

    extract client handling

--- lfault/__main__.py
+++ lfault/__main__.py
@@ -1,10 +1,14 @@
 from .server import listen
 
 
+def handle_client(client):
+    with client:
+        chunk = client.recv(4096)
+        print(repr(chunk))
+        print("Header terminator found:", b"\r\n\r\n" in chunk)
+
+
 with listen() as listener:
     while True:
         client, _ = listener.accept()
-        with client:
-            chunk = client.recv(4096)
-            print(repr(chunk))
-            print("Header terminator found:", b"\r\n\r\n" in chunk)
+        handle_client(client)
